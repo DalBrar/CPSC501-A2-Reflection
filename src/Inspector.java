b2150f030b8e0c9419c7023e547b70ade45b7a55
DalBrar <31530959+DalBrar@users.noreply.github.com>
2019-10-18 23:59:43 -0600

    completed Q5 & refactored getParameters() out from getConstructors()

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -25,7 +26,7 @@ public class Inspector {
     	int mods = c.getModifiers();
     	String modifiers = getModifiers(mods);
     	String name = c.getSimpleName();
-    	title = modifiers + name;
+    	title = modifiers + " " + name;
     	
     	// 2) Name of immediate super-class
     	title += getSuperClass(c);
@@ -104,6 +105,8 @@ public class Inspector {
     	if(Modifier.isSynchronized(mod))
     		out += "synchronized ";
     	
+    	if (out.length() > 0)
+    		out = out.substring(0, out.length()-1);
     	return out;
     }
 
@@ -136,27 +139,11 @@ public class Inspector {
     	output.add("\t// Constructors");
     	
     	Constructor[] constructors = c.getDeclaredConstructors();
-    	for(Constructor con : constructors)
+    	for(Constructor constr : constructors)
     	{
-    		String conOutput = "\t" + getModifiers(con.getModifiers());
-    		conOutput += name + "(";
-    		
-    		int numParams = con.getParameterCount();
-    		if (numParams == 0)
-    			conOutput += ") {}";
-    		else {
-    			Parameter[] params = con.getParameters();
-    			String conParams = "";
-    			for(Parameter conParam : params) {
-    				String type = conParam.getType().getSimpleName();
-    				String paramName = "";
-    				if (conParam.isNamePresent())
-    					paramName = " " + conParam.getName();
-    				conParams += type + paramName + ", ";
-    			}
-    			conParams = conParams.substring(0, conParams.length()-2);
-    			conOutput += conParams + ") {}";
-    		}
+    		String conOutput = "\t" + getModifiers(constr.getModifiers());
+    		conOutput += " " + name + getParameters(constr);
+    		conOutput += " " + getExceptions(constr);
     		output.add(conOutput);
     	}
     	return output;
@@ -165,8 +152,52 @@ public class Inspector {
     @SuppressWarnings("rawtypes")
 	private static List<String> getMethods(Class c) {
     	List<String> output = new ArrayList<String>();
+    	output.add("\t// Methods");
+    	Method[] methods = c.getDeclaredMethods();
+    	for(Method method : methods) {
+    		if (!Modifier.isPublic(method.getModifiers()))
+    			method.setAccessible(true);
+    		
+    		String line = "\t" + getModifiers(method.getModifiers());
+    		line += " " + method.getReturnType().getSimpleName();
+    		line += " " + method.getName() + getParameters(method);
+    		line += " " + getExceptions(method);
+    		output.add(line);
+    	}
     	return output;
     }
     
+    private static String getParameters(Executable exec) {
+		String output = "(";
+		
+		int numParams = exec.getParameterCount();
+		if (numParams == 0)
+			output += ")";
+		else {
+			for(Parameter param : exec.getParameters()) {
+				String type = param.getType().getSimpleName();
+				String paramName = "";
+				if (param.isNamePresent())
+					paramName = " " + param.getName();
+				output += type + paramName + ", ";
+			}
+			output = output.substring(0, output.length()-2);
+			output += ")";
+		}
+		return output;
+    }
+
+    private static String getExceptions(Executable exec) {
+    	String output = "";
+    	Class<?>[] exceps = exec.getExceptionTypes();
+    	if (exceps.length > 0)
+    		output += "throws ";
+    	for(Class<?> eclass : exceps) {
+    		output += eclass.getSimpleName() + ", ";
+    	}
+    	if (exceps.length > 0)
+    		output = output.substring(0, output.length()-2);
+    	return output;
+    }
     
 }
